2e68cab26fff7f7add61fabd1b895777b34e6d55
Martin Martinez Rivera <mrtnz.rvr@gmail.com>
2022-07-12 08:32:41 -0700

    Add tests, github workflow, and fix linter warnings.

--- src/main.rs
+++ src/main.rs
@@ -1,20 +1,42 @@
+//! Code to generate the rustlings course.
 mod rustlings;
 
+use std::path::Path;
+
 use anyhow::Result;
-use trane::course_builder::CourseBuilder;
 
 static AUTHORS: &str = "The Trane Project";
 
-fn main() -> Result<()> {
-    let curr_dir = std::env::current_dir()?;
-
-    let course_builders: Vec<CourseBuilder> = vec![
-        rustlings::course_builder(),
-    ];
+fn build_courses(library_root: &Path) -> Result<()> {
+    let course_builders = vec![rustlings::course_builder()];
 
     for course_builder in course_builders {
-        course_builder.build(&curr_dir)?;
+        course_builder.build(library_root)?;
         println!("Built {} course", course_builder.course_manifest.name);
     }
     Ok(())
+}
+
+fn main() -> Result<()> {
+    let curr_dir = std::env::current_dir()?;
+    build_courses(curr_dir.as_path())?;
+    Ok(())
+}
+
+#[cfg(test)]
+mod tests {
+    use trane::scheduler::ExerciseScheduler;
+
+    use crate::build_courses;
+
+    #[test]
+    fn open_library() -> anyhow::Result<()> {
+        let temp_dir = tempfile::TempDir::new()?;
+        let library_root = &temp_dir.path().to_path_buf();
+        build_courses(library_root)?;
+        let trane = trane::Trane::new(library_root.to_str().unwrap())?;
+        let batch = trane.get_exercise_batch(None)?;
+        assert!(!batch.is_empty());
+        Ok(())
+    }
 }
